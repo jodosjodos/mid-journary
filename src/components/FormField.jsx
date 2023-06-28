64670a10310a168674a9354b2d3b4fa6bba99d4c
jodosjodos <jeandedieu2030@gmail.com>
2023-06-28 15:07:54 +0200

    create post ui

---
+++ src/components/FormField.jsx
@@ -0,0 +1,43 @@
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  handleChange,
+  isSupriseMe,
+  handleSupriseMe,
+  value,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {isSupriseMe && (
+          <button
+            onClick={handleSupriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Suprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3 border"
+      />
+    </div>
+  );
+};
+
+export default FormField;
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import {surpriseMePrompts } from "../constant"
+
+
+export function getRandomPrompt(prompt){
+    const randomIndex=Math.floor(Math.random() * surpriseMePrompts .length);
+    const randomPrompt=surpriseMePrompts [randomIndex]
+    if(randomPrompt===prompt){
+        getRandomPrompt()
+    }
+    return randomPrompt
+}
